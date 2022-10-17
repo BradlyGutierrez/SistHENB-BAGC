9c9d8376ab99d169ebf57b79b06ada02467ea795
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2022-10-17 07:31:48 -0600

    Agregué la clase empleados

---
+++ src/dao/Empleados.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author isi7
+ */
+public class Empleados {
+    private int edad;
+    private String nombre;
+    private String cargo;
+    private double salario;
+
+    public Empleados() {
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+}
